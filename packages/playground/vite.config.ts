403cd5fe7ad6fd45a55cfc106c8638511a93fbeb
Doron Rosenberg <doronr@gmail.com>
2024-05-01 12:27:59 -0700

    make vite rebuild local package deps during dev

---
+++ packages/playground/vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import * as path from 'path';
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@pretzeljs/pretzeljs" : path.resolve(__dirname, "../pretzeljs/src/index.ts")
+    }
+  }
+})
